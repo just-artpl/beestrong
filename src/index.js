8d2415240a88412f6a5d9095a550cc19e9de0610
just-artpl <justyna@just-art.pl>
2020-06-10 11:17:18 +0200

    Configurated environment and start coding

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './styles/main.scss'
+import 'jquery'
+import 'popper.js'
+import 'bootstrap'
+import 'bootstrap/scss/bootstrap.scss'
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [
+                  {
+                    loader: 'url-loader',
+                    options: {
+                      limit: 8192,
+                    },
+                  },
+                ],
+            },
+        ]
+    }
+}
