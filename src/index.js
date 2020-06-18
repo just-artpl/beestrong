75c98c0141e6be43a9e4af4d5547e37e9c7515ba
just-artpl <justyna@just-art.pl>
2020-06-18 21:31:00 +0200

    encoded all content

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
-import './styles/main.scss'
 import 'jquery'
 import 'popper.js'
 import 'bootstrap'
 import 'bootstrap/scss/bootstrap.scss'
+import './styles/main.scss'
+
+$(document).ready(function(){
+    $('.navbar');
+});
+
+/*
+jQuery(function () {
+    jQuery('#nav-beestrong').click(function(){
+        jQuery('html, body').animate({
+            scrollTop: jQuery('#beestrong').offset().top
+        }, 800);
+    });
+    jQuery('#nav-pierzga').click(function(){
+        jQuery('html, body').animate({
+            scrollTop: jQuery('#pierzga').offset().top
+        }, 800);
+    });
+    jQuery('#nav-pylek').click(function(){
+        jQuery('html, body').animate({
+            scrollTop: jQuery('#pylek').offset().top
+        }, 800);
+    });
+    jQuery('#nav-kontakt').click(function(){
+        jQuery('html, body').animate({
+            scrollTop: jQuery('#kontakt').offset().top
+        }, 800);
+    });
+});*/
